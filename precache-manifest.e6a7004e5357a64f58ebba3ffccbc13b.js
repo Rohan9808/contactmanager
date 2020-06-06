041c0a249ce0f16297518e9fe7981f4b8ec6e03c
Rohan <rohanbisht.stltd@gmail.com>
2020-06-06 18:04:37 +0530

    Updates

---
+++ precache-manifest.e6a7004e5357a64f58ebba3ffccbc13b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050183ff5c4b91f1966112c26a2f5b9e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "dd071f75b216ed5ee6f0",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ebd5aa482a10576b5622",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "dd071f75b216ed5ee6f0",
+    "url": "/contactmanager/static/js/2.e5ea7726.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/contactmanager/static/js/2.e5ea7726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd5aa482a10576b5622",
+    "url": "/contactmanager/static/js/main.c1eb99de.chunk.js"
+  },
+  {
+    "revision": "22a1eb616042739cf5e8",
+    "url": "/contactmanager/static/js/runtime-main.900c19c8.js"
+  }
+]);
